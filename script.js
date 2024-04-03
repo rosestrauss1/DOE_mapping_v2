d16b21c256f558152e453e265c3a610f131dda3e
Rose Strauss <92963227+rosestrauss1@users.noreply.github.com>
2024-04-02 21:08:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close-button")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-// Example function to show the modal - you'll need to integrate this with your map
+// Placeholder function to show the modal - update as needed
+// This function should be called based on specific interactions you define
 function showModal() {
     modal.style.display = "block";
 }
