01b03324107b0505c5213d48b970d73713f3fea8
Rose Strauss <92963227+rosestrauss1@users.noreply.github.com>
2024-04-02 21:28:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close-button")[0];
+var infoCard = document.getElementById("infoCard"); // Get the information card
 
 span.onclick = function() {
     modal.style.display = "none";
@@ -11,8 +12,23 @@ window.onclick = function(event) {
     }
 }
 
-// Placeholder function to show the modal - update as needed
-// This function should be called based on specific interactions you define
 function showModal() {
     modal.style.display = "block";
 }
+
+// Function to show the information card
+function showCard() {
+    infoCard.style.display = "block";
+}
+
+// Function to hide the information card
+function hideCard() {
+    infoCard.style.display = "none";
+}
+
+// Add event listener to hide the card when clicking outside of it
+window.addEventListener('click', function(e) {
+    if (!document.getElementById('infoCard').contains(e.target)) {
+        hideCard();
+    }
+});
